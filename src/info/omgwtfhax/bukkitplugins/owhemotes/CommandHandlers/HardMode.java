2bf187ab875bede634e72da7892c3037fc33e2db
Tyler Hackett <ins10102@gmail.com>
2013-07-26 03:31:51 -0700

    Set up basics for HardMode class

--- src/info/omgwtfhax/bukkitplugins/owhemotes/CommandHandlers/HardMode.java
+++ src/info/omgwtfhax/bukkitplugins/owhemotes/CommandHandlers/HardMode.java
@@ -1,8 +1,14 @@
 package info.omgwtfhax.bukkitplugins.owhemotes.CommandHandlers;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import info.omgwtfhax.bukkitplugins.owhemotes.Emote;
 import info.omgwtfhax.bukkitplugins.owhemotes.OWHEmotes2_0;
 
 public class HardMode {
+	
+	org.bukkit.command.SimpleCommandMap cmap = null;
 	/*
 	 * In this class we do what we have to do to provide users the best plugin possible.
 	 * 
@@ -18,6 +24,43 @@ public class HardMode {
 	HardMode(OWHEmotes2_0 instance)
 	{
 		myPlugin = instance;
+		cmap = myPlugin.getCommandMap();
+	}
+	
+	//create an EmoteCommand from an emote
+	public void addEmote(Emote emote)
+	{
+		//TODO create new EmoteCommand & setup
+	}
+	
+	//unregister EmoteCommand from cmap
+	public void removeEmote(EmoteCommand emoteCommand)
+	{
+		if(cmap != null)
+			emoteCommand.unregister(cmap);
+	}
+	
+	private class EmoteCommand extends Command{		
+		//Used to create a command using the info of an emote
+		
+		org.bukkit.command.CommandExecutor exe = myPlugin;
+
+		protected EmoteCommand(Emote emote) 
+		{
+			super(emote.getCommand());
+			
+			if(cmap != null)
+			{
+				this.register(cmap);
+			}
+		}
+
+		@Override
+		public boolean execute(CommandSender sender, String commandLabel, String[] args) 
+		{				
+				return exe.onCommand(sender, this, commandLabel, args);
+		}
+		
 	}
 
 }

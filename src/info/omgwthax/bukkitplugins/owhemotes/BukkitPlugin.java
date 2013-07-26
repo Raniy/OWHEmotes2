937d90e4d93d435cf5e3e745ca41fee82853562f
Christopher Lohman <christopher_lohman@yahoo.com>
2013-07-26 02:24:18 -0500

    Some minor work in BukkitPlugin.

--- src/info/omgwthax/bukkitplugins/owhemotes/BukkitPlugin.java
+++ src/info/omgwthax/bukkitplugins/owhemotes/BukkitPlugin.java
@@ -1,5 +1,24 @@
 package info.omgwthax.bukkitplugins.owhemotes;
 
-public class BukkitPlugin {
+import org.bukkit.Bukkit;
 
+public class BukkitPlugin extends org.bukkit.plugin.java.JavaPlugin{
+	
+	// Wrap the built in Config.yml file
+	org.bukkit.configuration.file.FileConfiguration myConfig = null;
+	
+	// Wrap the broadcast to player function
+	boolean sendToPlayer(String player, String msg)
+	{
+		//Anytime we are working with Bukkit or CraftBukkit, do Error handling!!!	
+		try {
+			Bukkit.getServer().getPlayer(player).sendMessage(msg); // Send msg to player with that name.
+				
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false; // Somehow we were unable to send a message to the player!
+		}
+			return true;
+	}
+	
 }

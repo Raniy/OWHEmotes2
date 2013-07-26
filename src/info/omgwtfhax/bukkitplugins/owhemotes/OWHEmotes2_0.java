7c7671b53e8c9921b29de4128f5c7b5195feb393
Christopher Lohman <christopher_lohman@yahoo.com>
2013-07-26 03:43:11 -0500

    Some quick changes to the Main class for Insom.
    He is working on 'Hard Mode' and needed some stuff quickly.

--- src/info/omgwtfhax/bukkitplugins/owhemotes/OWHEmotes2_0.java
+++ src/info/omgwtfhax/bukkitplugins/owhemotes/OWHEmotes2_0.java
@@ -1,5 +1,7 @@
 package info.omgwtfhax.bukkitplugins.owhemotes;
 
+import java.util.List;
+
 import info.omgwtfhax.bukkitplugins.owhemotes.CommandHandlers.HardMode;
 import info.omgwtfhax.bukkitplugins.owhemotes.CommandHandlers.SoftMode;
 
@@ -9,9 +11,21 @@ public class OWHEmotes2_0 extends info.omgwtfhax.bukkitplugins.core.BukkitPlugin
 		HARD, SOFT; 
 	}
 	
+	// List of Emotes
+	private List<Emote> myEmotes = null;
+			
 	//Command Handlers
 	SoftMode cmdHandlerSoft = null;
 	HardMode cmdHandlerHard = null;
+	
+	// Getters and Setters
+	
+	public List<Emote> getMyEmotes() {
+		return myEmotes;
+	}
+	public void setMyEmotes(List<Emote> myEmotes) {
+		this.myEmotes = myEmotes;
+	}
 	
 	
 }

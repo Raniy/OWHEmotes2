98d413d8cd44b363f370e9877b2c5cf66e5d5875
Christopher Lohman <christopher_lohman@yahoo.com>
2013-07-26 19:46:16 -0500

    Working towards 0.2 Milestone. Putting around

---
+++ src/info/omgwtfhax/bukkitplugins/owhemotes/CommandHandlers/BaseCommands.java
@@ -0,0 +1,14 @@
+package info.omgwtfhax.bukkitplugins.owhemotes.CommandHandlers;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class BaseCommands implements org.bukkit.command.CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,	String[] arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
